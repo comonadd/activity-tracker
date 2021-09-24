9d131c69c96d7c4476069927d46d3e834e40d4b7
Dmitry Guzeev <dmitri.guzeev@gmail.com>
2021-09-25 01:07:42 +0300

    Start adding graphs

--- src/scenes/DayPage.tsx
+++ src/scenes/DayPage.tsx
@@ -34,6 +34,7 @@ import {
   colorGradient,
 } from "~/util";
 import { DataGrid } from "@mui/x-data-grid";
+import * as d3 from "d3";
 
 interface DayPageProps {
   year: string;
@@ -86,7 +87,7 @@ const TopSites = (props: { records: TrackInfoRecord[] }) => {
   }, [props.records]);
   const groupedBySiteSorted = new Map(
     Array.from(groupedBySite.entries()).sort(([_, a], [__, b]) =>
-      a.timeSpent > b.timeSpent ? -1 : a.timeSpent < b.timeSpent ? 1 : 0
+      a.timeSpent > b.timeSpent ? 1 : a.timeSpent < b.timeSpent ? -1 : 0
     )
   );
   const renderedItems = useMemo(() => {
@@ -99,7 +100,7 @@ const TopSites = (props: { records: TrackInfoRecord[] }) => {
       );
       return (
         <div className="top-sites-entry" key={siteName} style={{ background }}>
-          <span className="top-sites-entry__name">{siteName}: </span>
+          <span className="top-sites-entry__name">{siteName}</span>
           <span className="top-sites-entry__value">
             {toDuration(new Date(info.timeSpent))}
           </span>
@@ -162,6 +163,116 @@ const FullDayLog = (props: { records: TrackInfoRecord[] }) => {
   );
 };
 
+const MultilineChart = function <T>({
+  data,
+  dimensions,
+  getPointValue,
+  getPointX,
+}: {
+  data: T[];
+  dimensions: {
+    width: number;
+    height: number;
+    margin: { right: number; left: number; bottom: number; top: number };
+  };
+  getPointValue: (d: T) => number;
+  getPointX: (d: T) => Date;
+}) {
+  const svgRef = React.useRef(null);
+  const { width, height, margin } = dimensions;
+  const svgWidth = width + margin.left + margin.right;
+  const svgHeight = height + margin.top + margin.bottom;
+
+  React.useEffect(() => {
+    // If no data, don't render
+    if (data.length === 0) return null;
+
+    const xScale = d3
+      .scaleTime()
+      .domain(d3.extent(data, getPointX))
+      .range([0, width]);
+
+    const yScale = d3
+      .scaleLinear()
+      .domain([d3.min(data, getPointValue), d3.max(data, getPointValue)])
+      .range([height, 0]);
+
+    // Create root container where we will append all other chart elements
+    const svgEl = d3.select(svgRef.current);
+    svgEl.selectAll("*").remove(); // Clear svg content before adding new elements
+    const svg = svgEl
+      .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    // Add X grid lines with labels
+    const xAxis = d3
+      .axisBottom(xScale)
+      .ticks(5)
+      .tickSize(-height + margin.bottom);
+    const xAxisGroup = svg
+      .append("g")
+      .attr("transform", `translate(0, ${height - margin.bottom})`)
+      .call(xAxis);
+    xAxisGroup.select(".domain").remove();
+    xAxisGroup.selectAll("line").attr("stroke", "rgb(0, 0, 0)");
+    xAxisGroup
+      .selectAll("text")
+      .attr("color", "black")
+      .attr("font-size", "0.75rem");
+
+    // Add Y grid lines with labels
+    const yAxis = d3
+      .axisLeft(yScale)
+      .ticks(5)
+      .tickSize(-width)
+      .tickFormat((val) => `${val}%`);
+    const yAxisGroup = svg.append("g").call(yAxis);
+    yAxisGroup.select(".domain").remove();
+    yAxisGroup.selectAll("line").attr("stroke", "rgb(0, 0, 0)");
+    yAxisGroup
+      .selectAll("text")
+      .attr("color", "black")
+      .attr("font-size", "0.75rem");
+
+    svg
+      .selectAll(".line")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("x", (d, i) => i * 70)
+      .attr("y", (d, i) => height - getPointValue(d))
+      .attr("width", 25)
+      .attr("height", (d: T, i) => getPointValue(d))
+      .attr("fill", "green");
+  }, [data]);
+
+  return <svg ref={svgRef} width={svgWidth} height={svgHeight} />;
+};
+
+const DayGraph = (props: { records: TrackInfoRecord[] }) => {
+  const { config } = useContext(AppContext);
+  const { records } = props;
+  const container = React.useRef(null);
+  const color = "#00ff00";
+  const margin = { left: 15, right: 15, top: 15, bottom: 15 };
+  const width = 1000;
+  const height = 500;
+  return (
+    <div>
+      <MultilineChart
+        data={records}
+        dimensions={{
+          width: 1000,
+          height: 500,
+          margin: { left: 20, right: 20, bottom: 20, top: 20 },
+        }}
+        getPointValue={(p) => recordProd(config, p)}
+        getPointX={(p: TrackInfoRecord) => p.created}
+      />
+    </div>
+  );
+};
+
 const DayPage = (props: DayPageProps) => {
   const { year, month, day } = props;
   const { config } = useContext(AppContext);
@@ -177,7 +288,7 @@ const DayPage = (props: DayPageProps) => {
     })();
   }, [db]);
 
-  const [value, setValue] = React.useState<number>(0);
+  const [value, setValue] = React.useState<number>(2);
 
   const handleChange = (event: React.SyntheticEvent<any>, newValue: number) => {
     setValue(newValue);
@@ -193,18 +304,20 @@ const DayPage = (props: DayPageProps) => {
       <Typography component="h1" variant="h4">
         {dateToString(dayDate)}
       </Typography>
-      <div>This day has {records.length} records.</div>
-      <div>Productivity level: {prodLevel}.</div>
       <ProductivityLevel level={prodLevel} config={config} />
       <Tabs value={value} onChange={handleChange} aria-label="Day tabs">
         <Tab label="Full log" {...a11yProps(0)} />
         <Tab label="Top sites by usage" {...a11yProps(1)} />
+        <Tab label="Day graph" {...a11yProps(2)} />
       </Tabs>
       <TabPanel value={value} index={0}>
         <FullDayLog records={records} />
       </TabPanel>
       <TabPanel value={value} index={1}>
         <TopSites records={records} />
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        <DayGraph records={records} />
       </TabPanel>
     </Page>
   );
